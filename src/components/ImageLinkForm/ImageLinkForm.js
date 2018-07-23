13838312e852cc2a4da3beda6e57c432a2fe71bb
Ariel Blasi <exorcismo@gmail.com>
2018-07-23 18:09:23 -0300

    add form styles

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './ImagelinkForm.css'
 
 const ImageLinkForm = () => {
 	return (
@@ -6,7 +7,7 @@ const ImageLinkForm = () => {
 			<p className='f3'>
 				{'This artificial brain recognizes faces in your pictures, try it out!'}
 			</p>
-			<div className='center'>
+			<div className='center form pa4 br3 shadow-5'>
 				<input className='f4 pa2 w-70 center' type="text" name="" id=""/>
 				<button className='f4 w-30 grow link ph3 pv2 dib white bg-light-purple' >Detect</button>
 			</div>
