aaa107c68fda4320351d6bab1fe5fe14ac8a76b7
Ariel Blasi <exorcismo@gmail.com>
2018-07-23 19:00:25 -0300

    add Rank

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,6 +4,12 @@ import React from 'react'
 const Rank = () => {
 	return (
 		<div>
+			<div className='white f3'>
+				{'Benito Bellini, your current rank is...'}
+			</div>
+			<div className='white f1'>
+				{'#52'}
+			</div>
 		</div>
 	)
 }
