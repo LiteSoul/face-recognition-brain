8febaf0d343ef7b1353bb9765a843c59edf70986
litesoul <exorcismo@gmail.com>
2018-08-31 17:46:22 -0300

    logo styling

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,22 +1,10 @@
 import React from 'react'
-import Tilt from 'react-tilt'
+// import Tilt from 'react-tilt'
 import logo from './logo.png'
 
 const Logo = () => {
 	return (
-		<div>
-			<Tilt
-				className="Tilt"
-				options={{ max: 30 }}
-				style={{ height: 120, width: 150 }}
-			>
-				<div className="Tilt-inner pa2">
-					<img src={logo} alt="logo" />
-				</div>
-			</Tilt>
-		</div>
-
-		// <img src={brain} alt="brlogo" />
+		<img style={{ height: '80px', paddingLeft: '20px' }} src={logo} alt="Looks like...!" />
 	)
 }
 
