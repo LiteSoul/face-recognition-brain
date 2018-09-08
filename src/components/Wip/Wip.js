14be29302083df1a627c990d5e170d4582ef390f
litesoul <exorcismo@gmail.com>
2018-09-08 12:25:07 -0300

    padding fix

--- src/components/Wip/Wip.js
+++ src/components/Wip/Wip.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
 	return (
 		<div>
-			<p className="light-yellow pa5"><i>A *Work in Progress* on learning React and file manipulation, bugs possible,  more features to be added</i></p>
+			<p className="light-yellow pa4"><i>A *Work in Progress* on learning React and file manipulation, bugs possible,  more features to be added</i></p>
 		</div>
 	)
 }
